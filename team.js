a0f744ab3cc01ac19d05d8b55a0951a23e59c350
nicola1397 <nicola1397@outlook.it>
2024-01-23 16:22:12 +0100

    feat: DOM cards

--- team.js
+++ team.js
@@ -31,8 +31,34 @@ const team = [
   },
 ];
 
+const cardContainer = document.getElementById("cardContainer");
+
 for (let person of team) {
+  memberCard(person);
   console.log("Nome:" + person.name);
   console.log("Ruolo:" + person.role);
   console.log("Immagine:" + person.image);
+}
+
+function memberCard(person) {
+  const newDiv = document.createElement("div");
+  newDiv.classList.add("card");
+
+  const picture = document.createElement("img");
+  picture.src = `img/${person.image}`;
+  newDiv.appendChild(picture);
+  const bodyDiv = document.createElement("div");
+  bodyDiv.classList.add("card-body");
+  newDiv.appendChild(bodyDiv);
+  const title = document.createElement("h5");
+  title.classList.add("card-title");
+  bodyDiv.appendChild(title);
+
+  title.innerText = person.name;
+  const text = document.createElement("p");
+  text.classList.add("card-text");
+  text.innerText = person.role;
+  bodyDiv.appendChild(text);
+
+  cardContainer.appendChild(newDiv);
 }
